3e18bebea17bbfb26f4c6643e3ccd3c684aca8af
PuHuaizi <654195797@qq.com>
2019-05-06 17:43:33 +0800

    更新部分依赖

--- dubbo_provider/src/test/java/com/provider/test/ProviderTest.java
+++ dubbo_provider/src/test/java/com/provider/test/ProviderTest.java
@@ -1,11 +1,12 @@
 package com.provider.test;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
 public class ProviderTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BeansException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:springmvc.xml");
         context.start();
         System.out.println("Dubbo provider is start...");
